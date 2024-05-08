0e4d28c046dbfe98cfb96ace4f7a375e24f1e4ac
UsernameisKoo <149701913+UsernameisKoo@users.noreply.github.com>
2024-05-08 23:30:48 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Bronze/5086. 배수와 약수/배수와 약수.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(void)
+{
+	int m, n;
+	while (1)
+	{
+		scanf("%d %d", &m, &n);
+		if (m == 0 && n == 0)
+		{
+			break;
+		}
+		if (n % m == 0)
+		{
+			printf("factor\n");
+		}
+		else if (m % n == 0)
+		{
+			printf("multiple\n");
+		}
+		else {
+			printf("neither\n");
+		}
+
+
+	}
+	return 0;
+}
