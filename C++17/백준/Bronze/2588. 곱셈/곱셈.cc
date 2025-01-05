10fbd400b74214f1bd21789dba69c60ff466d9bd
KooSeoyon <149701913+UsernameisKoo@users.noreply.github.com>
2025-01-05 16:37:58 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+	int A, B;
+	scanf("%d\n%d", &A,&B);
+	printf("%d\n", A * (B % 10));
+	printf("%d\n", A * ((B % 100)/10));
+	printf("%d\n", A * (B / 100)); 
+	printf("%d", A * B );
+}
