0618fd10c91efad8de44d298597aff47899cc627
KooSeoyon <149701913+UsernameisKoo@users.noreply.github.com>
2025-05-22 02:35:41 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 48 ms, Memory: 3456 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<algorithm>
+#include<vector>
+using namespace std;
+void radix_sort(vector<pair<int, int>>& v);
+int n;
+
+void radix_sort(vector<pair<int, int>>& v)
+{
+	vector<pair<int, int>> output(n);
+	int count[200001] = { 0, };
+	for (int i = 0; i < n; i++)
+	{
+		count[v[i].first + 100000]++;
+	}
+	for (int i = 1; i < 200001; i++)
+	{
+		count[i]+=count[i-1];
+	}
+	for (int i = n - 1; i >= 0; i--)
+	{
+		output[--count[v[i].first + 100000]] = v[i];
+	}
+	v = output;
+
+	fill(count, count + 200001, 0);
+	for (int i = 0; i < n; i++)
+	{
+		count[v[i].second + 100000]++;
+	}
+	for (int i = 1; i < 200001; i++)
+	{
+		count[i] += count[i - 1];
+	}
+	for (int i = n - 1; i >= 0; i--)
+	{
+		output[--count[v[i].second + 100000]] = v[i];
+	}
+	v = output;
+}
+int main(void)
+{
+	scanf("%d", &n);
+
+	vector<pair<int, int>> v(n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d %d", &v[i].first, &v[i].second);
+	}
+	
+	radix_sort(v);
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d %d\n", v[i].first, v[i].second);
+	}
+}
