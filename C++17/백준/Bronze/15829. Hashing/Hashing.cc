fb4208f01c9156b940cccea5b74f01cd688fbdb7
KooSeoyon <149701913+UsernameisKoo@users.noreply.github.com>
2025-05-23 00:04:28 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1228 KB, Score: 100 point -BaekjoonHub

--- C++17/백준/Bronze/15829. Hashing/Hashing.cc
+++ C++17/백준/Bronze/15829. Hashing/Hashing.cc
@@ -3,10 +3,20 @@
 #include<cmath>
 #include<algorithm>
 using namespace std;
-long long h, result = 0;
+long long result = 0;
 int m = 1234567891, r = 31;
+
+long long recursive(long long h, int r, int i)
+{
+	if (i == 0)
+		return h;
+	h *= r;
+	h = h % m;
+	return recursive(h, r, i - 1);
+}
 int main(void)
 {
+	long long h = 1;
 	int l;
 	char c;
 	scanf("%d", &l);
@@ -19,8 +29,9 @@ int main(void)
 	}
 	for (int i = 0; i < l; i++)
 	{
-		h = pow(r, i);
+		h = 1;
+		h = recursive(h, r, i);
 		result += (v[i] * h) % m;
 	}
-	printf("%d", result);
+	printf("%lld", result%m);
 }
